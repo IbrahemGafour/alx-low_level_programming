27cb1ebb4c1b9e392a748ff9432cc0423c6f8d79
IbrahemGafour <ibrahem.abdelfatah.g4@gmail.com>
2023-03-24 19:58:38 -0700

    gfgf

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -11,11 +11,11 @@ int main(void)
 	int month;
 	int day;
 	int year;
-
-	month = 4;
-	day = 01;
-	year = 1997;
-
+/**
+	*month = 4;
+	*day = 01;
+	*year = 1997;
+*/
 	month = 2;
         day = 29;
         year = 2000;
