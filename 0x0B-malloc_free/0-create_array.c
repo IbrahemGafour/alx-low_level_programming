37f586bb1c83ffa7626f75b1c25ab99332342ccd
IbrahemGafour <IbrahemGafour>
2023-08-08 12:08:24 -0700

    zhe2t

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,7 +5,6 @@
  * *create_array - create array of size size and assign char c
  * @size: size of array
  * @c: char to initialize
- * Description: create array of size size and assign char c
  * Return: pointer to array, NULL if fail
  *
  */
