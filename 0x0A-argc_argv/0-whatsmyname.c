0c7ccc58ffb13234f92b13bab5742889d084e6d3
IbrahemGafour <ibrahem.abdelfatah.g4@gmail.com>
2023-10-02 16:16:39 -0700

    first 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+
+/**
+ * main - print the program name
+ * @argc: int
+ * @argv: list
+ *return:0 
+ */
+
 int main (int argc , char const *argv[])
 {
-(void)argc;
-printf("%s \n",argv[0]);
-return (0)
+	(void)argc;
+	printf("%s\n",argv[0]);
+	return (0)
 }
